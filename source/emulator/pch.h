cc5cd99a9b82120998043ec78d0c57376f71449d
mohaila <31441122+mohaila@users.noreply.github.com>
2025-12-08 16:57:56 -0500

    Remove SDL2 headers

--- source/emulator/pch.h
+++ source/emulator/pch.h
@@ -1,9 +1,4 @@
 #pragma once
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_net.h>
-#include <SDL2/SDL_ttf.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
